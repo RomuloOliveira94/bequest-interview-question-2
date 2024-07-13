fe784a6a8ce6c0b2be86f33548891b5da993b87e
Rômulo Augusto <romuloffall@gmail.com>
2024-07-13 00:23:22 -0300

    #adjust on block chain filepath

--- server/services/BackupBlockchainService.ts
+++ server/services/BackupBlockchainService.ts
@@ -1,10 +1,6 @@
 import { Block } from "../models/Block";
-import * as fs from 'fs';
-
-//Example backup file path, obviously this is going to be save in a more secure place
-const backupFilePath = '../db/blockchain-backup.json';
-//Example database file path, obviously this is going to be save in a more secure place
-const databaseFilePath = '../db/blockchain.json';
+import fs from 'fs';
+import { backupFilePath, blockchainFilePath } from "../shared/consts";
 
 export class BackupBlockchainService {
     public static createBackup(chain: Block[]): Block[] {
@@ -21,7 +17,7 @@ export class BackupBlockchainService {
         try {
             const backupData = fs.readFileSync(backupFilePath, 'utf-8');
             const chain: Block[] = JSON.parse(backupData);
-            fs.writeFileSync(databaseFilePath, JSON.stringify(chain, null, 2));
+            fs.writeFileSync(blockchainFilePath, JSON.stringify(chain, null, 2));
             return chain;
         } catch (error) {
             console.error('Error to restore the Backup', error);
