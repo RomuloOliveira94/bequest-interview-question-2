623cfa00c2a5ff903518e43025d20422b8dc8ba1
Rômulo Augusto <romuloffall@gmail.com>
2024-07-13 21:39:47 -0300

    #clean up

--- server/shared/consts.ts
+++ server/shared/consts.ts
@@ -1,6 +1,4 @@
 import path from "path";
 
-//Example backup file path, obviously this is going to be save in a more secure place
 export const backupFilePath = path.join(__dirname, '../db/blockchain-backup.json');
-//Example database file path, obviously this is going to be save in a more secure place
 export const blockchainFilePath = path.join(__dirname, '../db/blockchain.json');
